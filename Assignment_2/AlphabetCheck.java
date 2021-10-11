4fd2ea8ef5c83fc2b1db805050e239fefe6dfaf4
arvndx <iamarvind006@gmail.com>
2021-10-11 12:59:46 +0530

    second commit

--- Assignment_2/AlphabetCheck.java
+++ Assignment_2/AlphabetCheck.java
@@ -3,20 +3,20 @@ package Assignment_2;
 import java.util.*;
 public class AlphabetCheck {
     public  static boolean isAlphabets(String words){
-        boolean[] lowerCaseCount = new boolean[26];
+        boolean[] isCharPresent = new boolean[26];
 
         //pass to trace all the alphabets as per lowercase or uppercase
         for(char ch : words.toCharArray()){
             if(Character.isUpperCase(ch)){
-                lowerCaseCount[ch-'A'] = true;
+                isCharPresent[ch-'A'] = true;
             }else if(Character.isLowerCase(ch)){
-                lowerCaseCount[ch-'a'] = true;
+                isCharPresent[ch-'a'] = true;
             }
         }
 
         //pass to check whether all the alphabets is present in a String or not
         for(int i=0;i<26;i++){
-            if(!(lowerCaseCount[i])){
+            if(!(isCharPresent[i])){
                 return false;
             }
         }
